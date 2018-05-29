aede2e6f2b646c6e4d679043b08ab8b48283336a
Gabbi Forsythe <supergabbi18@gmail.com>
2018-05-29 17:57:40 -0400

    Added (broken) Mars Rover API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { Route, Switch, NavLink } from 'react-router-dom'
 
 import './App.css'
 import Github from './Github'
+import Nasa from './Nasa'
+import Homework from './Homework'
 
 
 class App extends Component {
@@ -15,16 +17,23 @@ class App extends Component {
         </div>
         <ul className="navLinks">
           <li>
-            <NavLink to="/Github">GitHub API</NavLink>
+            <NavLink to="/github">GitHub API</NavLink>
+          </li>
+          <li>
+            <NavLink to="/nasa">NASA API</NavLink>
+          </li>
+          <li>
+            <NavLink to="/homework">Homework</NavLink>
           </li>
         </ul>
         <Switch>
-          <Route path="/Github" component={Github} />
+          <Route path="/github" component={Github} />
+          <Route path="/nasa" component={Nasa} />
+          <Route path="/homework" component={Homework} />
           <Route render={() => <p>To get started, click one of the links above.</p>} />
         </Switch>
       </div>
     )
   }
 }
-
-export default App;
+export default App
