7700fdfd96051410b3376dfef5104f63bc2b8f26
Gabbi Forsythe <supergabbi18@gmail.com>
2018-05-29 23:31:22 -0400

    Add API functionality, display city name that matches zip code entered

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -20,18 +20,19 @@ class WeatherForecast extends Component {
     }
 
     fetchWeatherData = (props) => {
-        fetch(`api.openweathermap.org/data/2.5/weather?zip=${props.match.params.zipcode},us&APPID=c561e0162b310eb53aedd5b957d33d21`)
+        fetch(`https://api.openweathermap.org/data/2.5/weather?zip=${props.match.params.zipcode},us&APPID=c561e0162b310eb53aedd5b957d33d21`)
           .then(response => response.json())
-          .then(zipcode => this.setState({ zipcode }))
+          .then(weather => this.setState({ weather }))
           .catch((() => console.log('Well, shucks.')))
     }
 
     render () {
-        const { zipcode } = this.state
+        const { weather } = this.state
         console.log(this.state)
         return (
             <div className="WeatherForecast">
-                <h3>City: {zipcode}</h3>
+                <h2>{weather.name}</h2>
+                <h3>hi:</h3>
             </div>
         )
     }
